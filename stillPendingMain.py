d5c5c353d416da58d20e960382d058b2c4b59124
Osman Younas <osmanyounas@hotmail.com>
2018-05-11 12:37:27 +0200

    Add files via upload
    Oppdatert 8ball

--- stillPendingMain.py
+++ stillPendingMain.py
@@ -1,12 +1,24 @@
 import discord
+import random
+import asyncio
+import aiohttp
 from discord.ext import commands
 from discord.voice_client import VoiceClient
+from discord import Game
+from discord.ext.commands import Bot
+from discord.ext import commands
 
-startup_extensions = ["Music"]
-bot = commands.Bot("?")
+
+BOT_PREFIX = ("?", "!")
+TOKEN = 'NDA4NTY5NzUyMzExNTYyMjQw.DVYr0A.wvTcWnSsJvwNF-J_E9Q5dShhlg4'
+
+client = Bot(command_prefix=BOT_PREFIX)
 
 
-@bot.event
+startup_extensions = ['Music']
+
+
+@client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
@@ -14,16 +26,56 @@ async def on_ready():
     print('------')
 
 class Main_Commands():
-    def __init__(self,  bot):
-        self.bot = bot
+    def __init__(self,  client):
+        self.client = client
+
 
 if __name__ == "__main__":
     for extension in startup_extensions:
         try:
-            bot.load_extension(extension)
+            client.load_extension(extension)
         except Exception as e:
             exc = '{}:  {}'.format(type(e).__name__, e)
             print('failed to load extension {}\n{}'.format(extension, exc))
 
+@client.command(name='8ball',
+                description="Answers a yes/no question.",
+                brief="Answers from the beyond.",
+                aliases=['eight_ball', 'eightball', '8-ball'],
+                pass_context=True)
+async def eight_ball(context):
+    possible_responses = [
+        'That is a resounding no',
+        'It is not looking likely',
+        'Too hard to tell',
+        'It is quite possible',
+        'Definitely',
+    ]
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
 
-bot.run('NDA4NTY5NzUyMzExNTYyMjQw.DVYr0A.wvTcWnSsJvwNF-J_E9Q5dShhlg4')
+
+@client.command()
+async def roll(dice : str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await client.say('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await client.say(result)
+
+
+@client.command()
+async def add(left : int, right : int):
+    """Adds two numbers together."""
+    await client.say(left + right)
+
+@client.command(description='For when you wanna settle the score some other way')
+async def choose(*choices : str):
+    """Chooses between multiple choices."""
+    await client.say(random.choice(choices))
+
+
+client.run('NDA4NTY5NzUyMzExNTYyMjQw.DVYr0A.wvTcWnSsJvwNF-J_E9Q5dShhlg4')
